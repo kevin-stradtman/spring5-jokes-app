e701e8a6d301098c63d15e00d8cb8dd9439f7d83
Kevin Stradtman <kevin.stradtman@gmail.com>
2021-06-29 01:17:21 -0400

    Initial commit

---
+++ src/test/java/com/stradtman/spring5jokesapp/Spring5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stradtman.spring5jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
